f5c5c1773927e257ad1acdc321eaaa1ec7d3431d
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-06-02 00:48:49 +0300

    Update files

--- src/Root.js
+++ src/Root.js
@@ -4,10 +4,12 @@ import {Provider} from "react-redux";
 import {createStore, applyMiddleware, compose} from "redux";
 import reducers from "reducers";
 
+import reduxPromise from "redux-promise";
+
 export default ({children, initialState = {}}) => {
 
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware()));
+    const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(reduxPromise)));
 
     return (
         <Provider store={store}>
